6604b50b98e021d5c8844a6260a43d8ce39d31c2
plftch9 <plftch9@gmail.com>
2024-04-24 11:57:27 -0400

    implement selection sort

--- sortAlgorithms/src/InsertionSort.java
+++ sortAlgorithms/src/InsertionSort.java
@@ -1,3 +1,78 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import SortFunctions.Utils;
+
 public class InsertionSort {
 
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+
+            try {
+
+                int[] fileArray = Utils.loadArray(new File(args[0]));
+
+                Scanner input = new Scanner(System.in);
+                System.out.println("Welcome to insertion sort program");
+                System.out.println("1. Sort from small -> large");
+                System.out.println("2. Sort from large -> small");
+
+                int selection = input.nextInt();
+                input.close();
+
+                switch (selection) {
+                    case 1:
+                        insertionLarge(fileArray);
+                        Utils.displayResults(fileArray);
+                        break;
+
+                    case 2:
+                        insertionSmall(fileArray);
+                        Utils.displayResults(fileArray);
+                        break;
+
+                    default:
+                        System.out.println("That is not one of the options");
+
+                }
+            } catch (FileNotFoundException noFile) {
+                System.out.println("Provided argument cannot be found");
+                System.exit(0);
+            }
+        } else {
+            System.out.println("No argument provided. Exiting program");
+            System.exit(0);
+        }
+    }
+
+    public static void insertionLarge(int[] arr) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int newElement = arr[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && arr[i - 1] > newElement; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[i] = newElement;
+        }
+    }
+
+    public static void insertionSmall(int[] arr) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int newElement = arr[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && arr[i - 1] < newElement; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[i] = newElement;
+        }
+    }
 }
